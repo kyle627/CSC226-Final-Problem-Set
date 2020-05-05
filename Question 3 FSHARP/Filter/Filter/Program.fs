7f85fb9af67b465d9627f4bd6b0e63068a01d9b3
kyle627 <kyleevangelisto@gmail.com>
2020-05-05 18:22:11 -0400

    fixed side effect

--- Question 3 FSHARP/Filter/Filter/Program.fs
+++ Question 3 FSHARP/Filter/Filter/Program.fs
@@ -14,10 +14,10 @@ module printformat =
 
 
 module filter =
-
     let rec removeWhen items target =
         let filteredlist = items |> List.filter (fun x -> x <> target) //logic to filter the target word(s) out of the list, required!
-        printformat.format(filteredlist) //function call to format list, not required!
+        //printformat.format(filteredlist) //function call to format list, not required! (Potential Side effect! :0 )
+        filteredlist //returns filtered list doesnt require return keyword
 
 
 [<EntryPoint>]
@@ -32,7 +32,8 @@ let main argv =
     let target = Console.ReadLine() //required target!
     printf "\n\nList after the target word is removed (and formatted)\n"
 
-    filter.removeWhen fruits target //function call with two parameters, required! 
+    let filteredlist = filter.removeWhen fruits target //function call with two parameters, required! Stores returned list here.
+    printformat.format(filteredlist)
 
     exit 0 //Program Ends!
 
